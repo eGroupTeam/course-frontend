fd4dc81f546f3a5a7c7e4fb49f4ab410f1870a87
ginnypinjyunlin <egroup.intern.ginnylin@gmail.com>
2022-07-07 11:28:38 +0800

    add stock

--- course04/my-app/components/product/Product.tsx
+++ course04/my-app/components/product/Product.tsx
@@ -2,7 +2,7 @@ import {ProductType} from '../../interfaces/entities';
 const Product = (product:ProductType) => {
   return (
     <tr>
-      <td>{product.desc}</td><td>{product.price}</td><td>庫存量:{product.stock}</td><button>刪除</button>
+      <td>{product.desc}</td><td>{product.price}</td><td>{product.stock}</td><button>刪除</button>
     </tr>
   )
 }
