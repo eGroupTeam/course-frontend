dc697c2daac48334349338c0735505c36a903b3e
kevinlin3 <egroup.intern.kevinlin@gmail.com>
2022-07-09 14:48:25 +0800

    hw07 done

--- course07/my-app/components/product/ProductCreate.tsx
+++ course07/my-app/components/product/ProductCreate.tsx
@@ -1,5 +1,5 @@
 import styles from '/styles/Home.module.css';
-import {Product} from '../../interfaces/entities';
+import { Product } from '../../interfaces/entities';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import DialogContent from '@mui/material/DialogContent';
@@ -9,42 +9,44 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 
 type Props = {
-  addProduct(product:Product):void;
-  close():void;
-  open:boolean;
+  addProduct(product: Product): void;
+  close(): void;
+  open: boolean;
 }
 
-const ProductCreate:React.FC<Props> = (props) => {
+const ProductCreate: React.FC<Props> = (props) => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Product>();
   const onSubmit: SubmitHandler<Product> = product => {
     console.log("clicked");
     props.close();
-    props.addProduct(product); 
-    
+    props.addProduct(product);
+
   };
   const handleClose = () => {
     props.close();
   };
-   
+
   return (
     <div className={styles.container}>
-      
+
       <form onSubmit={handleSubmit(onSubmit)}>
-      <Dialog open={props.open} onClose={handleClose}>
-      <DialogTitle>新增產品</DialogTitle>
-      <DialogContent>
-      <TextField id="filled-basic" label="產品描述" variant="outlined" {...register("desc",{ required: true, minLength: 5 })}/><br/>
-        {errors.desc && <span>描述至少5個字<br/></span>}
-        <TextField id="filled-basic" label="產品價格" variant="outlined" type="number" {...register("price",{min:0, max:100000})}/><br/>
-        {errors.price && <span>價格在0到100000之間<br/></span>}
-      </DialogContent>
-      <DialogActions>
-        <Button variant="contained" onClick={handleSubmit(onSubmit)}>送出</Button>
-        <Button color="secondary" variant="contained" onClick={handleClose}>取消</Button>
-      </DialogActions>
-      </Dialog>
+        <Dialog open={props.open} onClose={handleClose}>
+          <DialogTitle>新增產品</DialogTitle>
+          <DialogContent>
+            <TextField id="filled-basic" label="產品描述" variant="outlined" {...register("desc", { required: true, minLength: 5 })} /><br />
+            {errors.desc && <span>描述至少5個字<br /></span>}
+            <TextField id="filled-basic" label="產品價格" variant="outlined" type="number" {...register("price", { min: 0, max: 100000 })} /><br />
+            {errors.price && <span>價格在0到100000之間<br /></span>}
+            <TextField id="filled-basic" label="庫存量" variant="outlined" type="number" {...register("stock", { min: 0 })} /><br />
+            {errors.stock && <span>數量必須大於0<br /></span>}
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" onClick={handleSubmit(onSubmit)}>送出</Button>
+            <Button color="secondary" variant="contained" onClick={handleClose}>取消</Button>
+          </DialogActions>
+        </Dialog>
       </form>
-      
+
     </div>
   )
 }
--- course07/my-app/components/product/ProductListItem.tsx
+++ course07/my-app/components/product/ProductListItem.tsx
@@ -4,20 +4,22 @@ import { Button, TableCell } from "@mui/material";
 import TableRow from "@mui/material/TableRow";
 
 type Props = {
-  desc:string,
-  price:number,
-  index:number,
-  deleteProduct(index:number):void;
+  desc: string,
+  price: number,
+  index: number,
+  stock:number,
+  deleteProduct(index: number): void;
 }
 
-const ProductListItem:React.FC<Props> = (props) => {
-  const deleteProduct = ()=>{
+const ProductListItem: React.FC<Props> = (props) => {
+  const deleteProduct = () => {
     props.deleteProduct(props.index)
   }
   return (
     <TableRow>
-      <TableCell >{props.desc}</TableCell>
+      <TableCell>{props.desc}</TableCell>
       <TableCell>{props.price}</TableCell>
+      <TableCell>{props.stock}</TableCell>
       <TableCell><Button variant="contained" onClick={deleteProduct}>刪除</Button></TableCell>
     </TableRow>
   )
