89ac6976dbebdb186872a2a39a6371da3e959463
ginnypinjyunlin <egroup.intern.ginnylin@gmail.com>
2022-07-10 01:29:09 +0800

    add stock

--- course07/my-app/components/product/ProductCreate.tsx
+++ course07/my-app/components/product/ProductCreate.tsx
@@ -16,7 +16,7 @@ type Props = {
 
 const ProductCreate:React.FC<Props> = (props) => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Product>();
-  const onSubmit: SubmitHandler<Product> = product => {
+  const onSubmit: SubmitHandler<Product> = (product) => {
     console.log("clicked");
     props.close();
     props.addProduct(product); 
@@ -37,6 +37,8 @@ const ProductCreate:React.FC<Props> = (props) => {
         {errors.desc && <span>描述至少5個字<br/></span>}
         <TextField id="filled-basic" label="產品價格" variant="outlined" type="number" {...register("price",{min:0, max:100000})}/><br/>
         {errors.price && <span>價格在0到100000之間<br/></span>}
+        <TextField id="filled-basic" label="產品庫存" variant="outlined" type="number" {...register("stock",{min:0})}/><br/>
+        {errors.stock && <span>庫存須大於等於0個<br/></span>}
       </DialogContent>
       <DialogActions>
         <Button variant="contained" onClick={handleSubmit(onSubmit)}>送出</Button>
