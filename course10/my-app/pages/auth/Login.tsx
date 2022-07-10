6aaf6fb9ffd4b1cf174ef66f66114830f3a1110d
Ben Wu <jitsung.wu@gmail.com>
2022-07-10 13:07:43 +0800

    course 10 & course 11(draft)

---
+++ course10/my-app/pages/auth/Login.tsx
@@ -0,0 +1,49 @@
+import Button from '@mui/material/Button';
+import {Login as User} from '@/interfaces/entities';
+import { useForm, SubmitHandler } from "react-hook-form";
+import Menu from '@/components/ui/Menu';
+import styles from '/styles/Home.module.css';
+/*
+import {AuthContext, AUTH_STATUS} from '@/components/auth/AuthContext';
+import { useContext } from 'react';
+*/
+import { useAppDispatch } from '@/components/auth/hooks'
+import { logout, login } from '@/components/auth/authSlice'
+
+import { useRouter } from 'next/router';
+import TextField from '@mui/material/TextField';
+
+
+const LoginPage = () => {
+  //const authContext = useContext(AuthContext);
+  const dispatch = useAppDispatch();
+
+  const router = useRouter();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<User>();
+  const onSubmit: SubmitHandler<User> = user => {
+    if (user.id === "benwu" && user.password === "ilovenext"){
+      //authContext.setStatus(AUTH_STATUS.LOGIN);
+      dispatch(login());
+    }
+    else {
+      //authContext.setStatus(AUTH_STATUS.LOGOUT);
+      dispatch(logout());
+    }
+    router.push("/product/test");
+  };
+  return(
+    <div className={styles.container}>
+      <Menu/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField id="filled-basic" label="帳號" variant="outlined" {...register("id",{ required: true, minLength: 5 })}/><br/>
+        {errors.id && <span>帳號至少5個字<br/></span>}
+        <TextField id="filled-basic" label="密碼" variant="outlined" type="password" {...register("password",{ required: true, minLength: 5 })}/><br/>
+        {errors.password && <span>密碼至少5個字<br/></span>}
+        <Button type = "submit" variant="contained">送出</Button>
+      </form>
+
+    </div>
+    )
+}
+
+export default LoginPage;
---
+++ course10/my-app/pages/typescript.tsx
@@ -0,0 +1,140 @@
+import type { NextPage } from 'next'
+import styles from '/styles/Home.module.css'
+
+//基本資料型態
+let heading: string="ben";//explicit type
+heading = "Wu";
+let heading2="I am ben";//implicit type
+//heading2 = 100.00; //Error: Type 'number' is not assignable to type 'string'
+
+//陣列
+const names: string[] = ["Mary","Tom"];
+names.push("Ben");
+//console.log(names);
+//names.push(12);//Argument of type 'number' is not assignable to parameter of type 'string'.
+//console.log(names);
+
+//物件
+/*
+const product: { type: string, price: number, desc:string } = {
+  //Property 'desc' is missing in type '{ type: string; price: number; }' but required in type '{ type: string; price: number; desc: string; }'
+  type: "iPhone",
+  price: 12000
+}
+*/
+
+const product: { type: string, price: number, desc?:string } = {
+  //desc是optional
+  type: "iPhone",
+  price: 12000
+}
+
+
+//物件陣列
+//desc是optional
+const products: { type: string, price: number, desc?:string }[] = 
+[
+  {
+    type: "iPhone",
+    price: 12000
+  },
+  {
+    type: "iPad",
+    price: 15000,
+    desc: "Apple iPad plus"
+  }
+];
+
+//自訂資料型態
+type Product={ type: string, price: number, desc?:string };
+const allProducts:Product [] = 
+[
+  {
+    type: "iPhone",
+    price: 12000
+  },
+  {
+    type: "iPad",
+    price: 15000,
+    desc: "Apple iPad plus"
+  }
+];
+
+//interface
+interface Book { type: string, price: number, desc?:string };
+const allBooks:Book [] = 
+[
+  {
+    type: "iPhone",
+    price: 12000
+  },
+  {
+    type: "iPad",
+    price: 15000,
+    desc: "Apple iPad plus"
+  }
+];
+
+function getInterest(balance:number):number{
+  return balance * .006;
+}
+
+
+
+//傳入參數一定要有資料型態 回傳值可以不用
+
+function testAny(option:number){
+  if (option===0){
+    return "test";
+  }
+  else {
+    return 0;
+  }
+
+}
+
+//type union && typeof
+function add(first:string | number, second:string | number): number{
+  
+  if (typeof first === "string"){
+    first = Number(first);
+  }
+  if (typeof second === "string"){
+    second = Number(second);
+  }
+
+  return first + second;
+
+}
+
+enum AccountType {
+  regular, saving
+}
+function checkAccountType(type: AccountType){
+  switch(type){
+    case AccountType.regular:
+      return "Regular Account";
+    case AccountType.saving:
+      return "Saving Account";
+    default:
+      return "Regular Account";
+  }
+}
+
+
+
+const TestPage: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>{heading}</h1>
+      <h2>{heading2}</h2>
+      {getInterest(10000)}<br/>
+      {checkAccountType(AccountType.regular)}<br/>
+      {add("1","2")}<br/>
+      {add(1,2)}<br/>
+      {add(1,"2")}<br/>
+    </div>
+  )
+}
+
+export default TestPage
