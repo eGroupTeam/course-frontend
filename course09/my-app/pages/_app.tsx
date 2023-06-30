2672f070c4c8276d134e022c5a3ccc9975a4d3c4
jimmy614 <egroup.intern.jimmyhsu@gmail.com>
2023-06-30 17:38:59 +0800

    feat: hw

--- course09/my-app/pages/_app.tsx
+++ course09/my-app/pages/_app.tsx
@@ -1,20 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-//import {AuthContext, AUTH_STATUS} from '@/components/auth/AuthContext';
+import {AuthContext, AUTH_STATUS} from '@/components/auth/AuthContext';
 //import {AuthReduxContext, AUTH_STATUS} from '@/components/auth/AuthReduxContext';
 import {store} from '@/components/auth/store'
 import { Provider } from 'react-redux'
 import { useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  //const [status, setStatus] = useState<AUTH_STATUS>(AUTH_STATUS.LOGOUT);
+  const [status, setStatus] = useState<AUTH_STATUS>(AUTH_STATUS.LOGOUT);
   return (
-    <Provider store={store}>  
+    <AuthContext.Provider value={{status, setStatus}}>
       <Component {...pageProps} />
-    </Provider>
+      </AuthContext.Provider>
 
-  )
+)
 
 }
 
 export default MyApp
+
+/*
+    <Provider store={store}>  
+      <Component {...pageProps} />
+    </Provider>
+*/
--- course09/my-app/pages/product/test.tsx
+++ course09/my-app/pages/product/test.tsx
@@ -2,13 +2,13 @@ import Menu from '@/components/ui/Menu';
 import Button from '@mui/material/Button';
 import { useRouter } from 'next/router';
 import styles from '/styles/Home.module.css';
-/*
+
 import {AuthContext, AUTH_STATUS} from '@/components/auth/AuthContext';
 import { useContext } from 'react';
-*/
+/*
 import { useAppSelector } from '@/components/auth/hooks'
 import {AUTH_STATUS} from '@/components/auth/authSlice';
-
+*/
 const Test = () => {
   const router = useRouter();
   const id = router.query.id;
@@ -18,13 +18,14 @@ const Test = () => {
   const login = () =>{
     router.push("/auth/Login");
   }
-  //const authContext = useContext(AuthContext);
-  const status = useAppSelector((state) => state.status.value)
+  const authContext = useContext(AuthContext);
+  //const status = useAppSelector((state) => state.status.value)
   return(
     <div className={styles.container}>
       <Menu/>
       {id}<br/>
-      {status === AUTH_STATUS.LOGIN ?"已登入": "未登入"}<br/>
+      {sessionStorage.getItem("name")}<br/>
+      {authContext.status === AUTH_STATUS.LOGIN? "已登入": "未登入"}<br/>
       <Button onClick={login}>回登入頁</Button>
       <Button onClick={action}>回上頁</Button>
     </div>
@@ -32,7 +33,7 @@ const Test = () => {
 }
 
 /*
-      {authContext.status === AUTH_STATUS.LOGIN? "已登入": "未登入"}<br/>
+ {status === AUTH_STATUS.LOGIN ?"已登入": "未登入"}<br/>
 */
 
 export default Test;
